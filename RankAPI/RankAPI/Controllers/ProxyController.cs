5e0c0b43281154e6f0178591b137428a70b87648
JacobXing <37539965+JacobXing@users.noreply.github.com>
2018-04-22 14:51:42 +1000

    Rank

---
+++ RankAPI/RankAPI/Controllers/ProxyController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MvcAngular;
+using RankAPI.Dtos;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Serilog;
+
+namespace RankAPI
+{
+    [Angular, Route("[controller]/[action]")]
+    public class ProxyController : Controller
+    {
+        private readonly AzureMLRequestOptions _azureMLRequestOptions;
+        private readonly AzureMLRequestOptions _logger;
+        public ProxyController(AzureMLRequestOptions azureMLRequestOptions, ILogger logger)
+        {
+            _azureMLRequestOptions = azureMLRequestOptions;
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<AzureMLRankResponse> Infer([FromBody] AzureMLRankRequest azureMLRankRequest)
+        {
+            _logger.Information("REquest Object is ($request)", azureMLRankRequest);
+            using (HttpClient httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {_azureMLRequestOptions.Key}");
+                var content = new StringContent(JsonConvert.SerializeObject(azureMLRankRequest), Encoding.UTF8, "application/json");
+                var response = await httpClient.PostAsync(_azureMLRequestOptions.Url, content);
+                var json = await response.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<AzureMLRankResponse>(json);
+                return result;
+            }
+        }
+    }
+}
